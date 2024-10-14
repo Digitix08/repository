aa4eb268aba120c0d6a189b653cfcc55b3d931b5
Digitix08 <115367839+Digitix08@users.noreply.github.com>
2024-10-14 14:01:29 +0300

    Update dark.js

--- dark.js
+++ dark.js
@@ -1,4 +1,4 @@
-window.onload = checkPrompt();
+document.body.onload = checkPrompt();
 function setCookie(cname, cvalue, exdays){
 	const d = new Date();
 	d.setTime(d.getTime() + (exdays*24*60*60*1000));
