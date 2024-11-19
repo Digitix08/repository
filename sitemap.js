13bca0b48302e93263ef3360d0c49a1797ba6b7a
Digitix08 <dg.ajtx@gmail.com>
2024-11-19 21:24:23 +0200

    changed like 3 files and apparently deleted like 20

---
+++ sitemap.js
@@ -0,0 +1,117 @@
+﻿function subfolder(){
+	sel3remove();
+	var sel2 = document.getElementById("Select2");
+	if(sel2.value == "Snow%20boarder%20v1.0_web/")snowboarder();
+	else if(sel2.value == "delivery%20driver%201.1_web/")delidriver();
+	else if(sel2.value == "smartkey_mobile/")smartkeymobile();
+}
+function sel3remove(){
+	if(document.getElementById("sel3"))
+		{
+			var sel3 = document.getElementById("sel3");
+			sel3.remove();
+		}
+}
+function slash(){
+	var selectElement = document.getElementById("Select2")
+	selectElement.innerHTML = "";
+	selectElement.add(new Option ("404.html"));
+	selectElement.add(new Option ("MINECRAFT.htm"));
+	selectElement.add(new Option ("chrome%20urls.html"));
+	selectElement.add(new Option ("controls.html"));
+	selectElement.add(new Option ("google44d0c4e00351c23e.html"));
+	selectElement.add(new Option ("index-old.htm"));
+	selectElement.add(new Option ("index.htm"));
+	selectElement.add(new Option ("index_mobile.html"));
+	selectElement.add(new Option ("info2.html"));
+	selectElement.add(new Option ("photo_gallery.htm"));
+	selectElement.add(new Option ("picviewer.html"));
+	selectElement.add(new Option ("settings.html"));
+	selectElement.add(new Option ("sorry.html"));
+	selectElement.add(new Option ("test.html"));
+	selectElement.add(new Option ("useful_links.html"));
+}
+function bview(){
+	var selectElement = document.getElementById("Select2")
+	selectElement.innerHTML = "";
+	selectElement.add(new Option ("MINECRAFT.htm"));
+	selectElement.add(new Option ("index.html"));
+	selectElement.add(new Option ("useful_links.html"));
+}
+function download(){
+	var selectElement = document.getElementById("Select2")
+	selectElement.innerHTML = "";
+	selectElement.add(new Option ("download_android_apps.htm"));
+	selectElement.add(new Option ("download_win_mobile_sofware.htm"));
+	selectElement.add(new Option ("download_windows_software.htm"));
+	selectElement.add(new Option ("index.htm"));
+}
+function games(){
+	var selectElement = document.getElementById("Select2");
+	selectElement.innerHTML = "";
+	selectElement.add(new Option ("Snow%20boarder%20v1.0_web/"));
+	selectElement.add(new Option ("delivery%20driver%201.1_web/"));
+	selectElement.add(new Option ("index.html"));
+}
+function snowboarder(){
+	const newSelect = document.createElement("select");
+	newSelect.id= "sel3";
+	newSelect.innerHTML = "";
+	newSelect.add(new Option ("index.html"));
+	document.getElementById("selectarea").append(newSelect);
+}
+function delidriver(){
+	const newSelect = document.createElement("select");
+	newSelect.id= "sel3";
+	newSelect.innerHTML = "";
+	newSelect.add(new Option ("index.html"));
+	document.getElementById("selectarea").append(newSelect);
+}
+function indexcasete(){
+	var selectElement = document.getElementById("Select2");
+	selectElement.innerHTML = "";
+	selectElement.add(new Option ("index.html"));
+}
+function loginregister(){
+	var selectElement = document.getElementById("Select2");
+	selectElement.innerHTML = "";
+	selectElement.add(new Option ("login.html"));
+	selectElement.add(new Option ("new_page_2.htm"));
+	selectElement.add(new Option ("user_registration.htm"));
+}
+function school(){
+	var selectElement = document.getElementById("Select2");
+	selectElement.innerHTML = "";
+	selectElement.add(new Option ("Componentele%20hardware%20ale%20unui%20pc.html"));
+	selectElement.add(new Option ("Cr&#259;ciun(1).html"));
+	selectElement.add(new Option ("Cr&#259;ciun.html"));
+	selectElement.add(new Option ("STOP.html"));
+	selectElement.add(new Option ("book12.html"));
+	selectElement.add(new Option ("cpp-vii.html"));
+	selectElement.add(new Option ("cpp-viii.html"));
+	selectElement.add(new Option ("câni.html"));
+	selectElement.add(new Option ("horar%20gabi(1).html"));
+	selectElement.add(new Option ("horar%20gabi.html"));
+	selectElement.add(new Option ("index.htm"));
+	selectElement.add(new Option ("new%201.html"));
+	selectElement.add(new Option ("romani_vreme_zoo.html"));
+	selectElement.add(new Option ("typop%20retelewelo%20de%20cdwjdfh.html"));
+}
+function shop(){
+	var selectElement = document.getElementById("Select2");
+	selectElement.innerHTML = "";
+	selectElement.add(new Option ("smartkey_mobile/"));
+	selectElement.add(new Option ("index.html"));
+}
+function smartkeymobile(){
+	const newSelect = document.createElement("select");
+	newSelect.id= "sel3";
+	newSelect.innerHTML = "";
+	newSelect.add(new Option ("index.html"));
+	document.getElementById("selectarea").append(newSelect);
+}
+function weirdram(){
+	var selectElement = document.getElementById("Select2");
+	selectElement.innerHTML = "";
+	selectElement.add(new Option ("index.html"));
+}
