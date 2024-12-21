ee4522d811a28fc13ea48865f9424c20a540b29d
Digitix08 <dg.ajtx@gmail.com>
2024-12-21 20:16:18 +0200

    testing redirect fix

---
+++ anchor.js
@@ -0,0 +1,7 @@
+﻿//this script is meant to detect site path for mobile/desktop redirects
+//DO NOT MOVE FROM ROOT OF (sub)SITE
+function rootcheck(){
+	var path = window.location.pathname;
+	path = path.substring(0, path.length - 9);
+	return path;
+}
