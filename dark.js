564c0934f7f67af9c89eb056919a1b67c3831f72
Digitix08 <dg.ajtx@gmail.com>
2025-03-04 22:08:22 +0200

    fixed errors, updated site

--- dark.js
+++ dark.js
@@ -1,6 +1,6 @@
-document.onreadystatechange = function (){if (document.readyState === 'complete')checkPrompt();}
+document.onreadystatechange = function (){if (document.readyState === 'complete')startChecks();}
 function checkBody() {
-	if(document.body) {console.log("correct");checkPrompt();}
+	if(document.body) {console.log("correct");startChecks();}
 	else {console.log("no");setTimeout(checkPrompt(), 3000);}
 };
 function checkPrompt() {
@@ -16,8 +16,31 @@ function checkPrompt() {
 		document.getElementById("prompt").className= "prompt";
 	}
 };
+function tabCheck(){
+	if(window.innerWidth < 995&&document.getElementsByClassName("menu").length>0){console.log("narrow view (" + window.innerWidth + "px)")
+		var n=document.getElementsByClassName("menu"),m=document.getElementsByClassName("menucurrent"),i
+		if (!Math.trunc) {
+		Math.trunc = function (v) {
+			return v < 0 ? Math.ceil(v) : Math.floor(v);
+			};
+		}
+		var nr=Math.trunc(window.innerWidth/6)-20 + "px"
+		console.log(nr);console.log(n[5].innerHTML);
+		for(i=2; i<n.length; i++)n[i].style.width=nr
+		m[0].style.width=nr
+		if(n[5].innerHTML=="Minecraft srvrs")n[5].innerHTML="MC srv";else if(n[6].innerHTML=="Minecraft srvrs")n[6].innerHTML="MC srv";else if(m[0].innerHTML=="Minecraft srvrs")m[0].innerHTML="MC srv"
+		if(window.innerWidth <= 700&&n[6].innerHTML=="Photo Gallery")n[6].innerHTML="Gallery"
+		if(window.innerWidth < 400) if(n[5].innerHTML=="Minecraft srvrs"||n[5].innerHTML=="MC srv")n[5].innerHTML="MC"; else if(m[0].innerHTML=="Minecraft srvrs"||m[0].innerHTML=="MC srv")m[0].innerHTML="MC"
+		if(m[0].innerHTML=="Index casete")m[0].innerHTML="Casete"
+	}
+}
+function startChecks(){
+	console.log("page start code, width is " + window.innerWidth); 
+	checkPrompt()
+	tabCheck()
+}
 function setCookie(cname, cvalue, exdays){
-	if (exdays.toString().indexOf("actualdate+") != -1){
+	if (exdays!=null&&exdays.toString().indexOf("actualdate+") != -1){
 		console.log("actual date");
 		exdays = exdays.substring(11)
 		console.log(exdays);
@@ -63,11 +86,49 @@ function promptRemove(yes) {
 	}
 	else {
 		document.getElementById("prompt").outerHTML = "";
-    }
+	}
 };
 function tab(e){
 	var tabPressed = false;
 	if(e.keyCode == 9) {
-        tabPressed = true;   
-    }
+		tabPressed = true;
+	}
+}
+function FP_swapImg() {//v1.0
+	var doc=document,args=arguments,elm,n;
+	doc.$imgSwaps=new Array();
+	for(n=2; n<args.length;n+=2) {
+		elm=FP_getObjectByID(args[n]);
+		if(elm) {
+			doc.$imgSwaps[doc.$imgSwaps.length]=elm;
+			elm.$src=elm.src;
+			elm.src=args[n+1];
+		}
+	}
+}
+
+function FP_getObjectByID(id,o) {//v1.0
+	var c,el,els,f,m,n;
+	if(!o)o=document;
+	if(o.getElementById) el=o.getElementById(id);
+	else if(o.layers) c=o.layers;
+	else if(o.all) el=o.all[id];
+	if(el) return el;
+	if(o.id==id || o.name==id) return o;
+	if(o.childNodes) c=o.childNodes;
+	if(c)
+		for(n=0; n<c.length; n++) {
+			el=FP_getObjectByID(id,c[n]);
+			if(el) return el;
+		}
+	f=o.forms;
+	if(f)
+		for(n=0; n<f.length; n++) {
+			els=f[n].elements;
+			for(m=0; m<els.length; m++){
+				el=FP_getObjectByID(id,els[n]);
+				if(el) return el;
+			} 
+		}
+	return null;
 }
